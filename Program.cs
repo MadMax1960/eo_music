2e079429d113d9395535fb74857314499db10488
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2023-07-25 11:07:16 -0400

    - Exports a json with the fsb header detected Hopefully this will be able to output all the headers detected so I can use it to patch them all :(

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using AssetsTools.NET.Extra;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Linq;
+using Newtonsoft.Json;
+using System.Xml;
 
 namespace eo_music
 {
@@ -25,6 +27,36 @@ namespace eo_music
                 res = Console.ReadLine().Replace("\"", "");
             }
             return res;
+        }
+
+        static List<FSBHeaderInfo> GetFSBHeadersInfo(byte[] data)
+        {
+            List<FSBHeaderInfo> headers = new List<FSBHeaderInfo>();
+            int index = 0;
+            while (index < data.Length - 3)
+            {
+                if (data[index] == FSB5_HEADER[0] &&
+                    data[index + 1] == FSB5_HEADER[1] &&
+                    data[index + 2] == FSB5_HEADER[2] &&
+                    data[index + 3] == FSB5_HEADER[3])
+                {
+                    var headerInfo = new FSBHeaderInfo
+                    {
+                        Offset = index,
+                        Size = BitConverter.ToUInt32(data, index + 4),
+                        NumSamples = BitConverter.ToUInt32(data, index + 16),
+                        SampleRate = BitConverter.ToUInt32(data, index + 44),
+                        NumChannels = BitConverter.ToUInt32(data, index + 48)
+                    };
+                    headers.Add(headerInfo);
+                    index += (int)headerInfo.Size;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            return headers;
         }
 
         static bool RemuxToFSB(string input, string output, int? loop_start = null, int? loop_end = null)
@@ -51,7 +83,7 @@ namespace eo_music
         static bool ConvertFile(string input, string output)
         {
             AudioInfo original_audio_info = GetAudioInfo(input);
-            if(original_audio_info.sample_rate == TARGET_SAMPLE_RATE && Path.GetExtension(input) == ".ogg")
+            if (original_audio_info.sample_rate == TARGET_SAMPLE_RATE && Path.GetExtension(input) == ".ogg")
             {
                 File.Copy(input, output, true);
                 return File.Exists(output);
@@ -68,6 +100,15 @@ namespace eo_music
             p.StandardOutput.ReadToEnd();
 
             return true;
+        }
+
+        struct FSBHeaderInfo
+        {
+            public int Offset;
+            public uint Size;
+            public uint NumSamples;
+            public uint SampleRate;
+            public uint NumChannels;
         }
 
         static AudioInfo GetAudioInfo(string input)
@@ -125,7 +166,7 @@ namespace eo_music
 
                 var baseField = am.GetTypeInstance(og_assetInst, asset).GetBaseField();
                 song_name = baseField.Get("m_Name").GetValue().AsString();
-                
+
                 Console.WriteLine($"Found AudioClip {baseField.Get("m_Name").GetValue().AsString()}! Setting the proper information...");
                 AssetTypeValueField resource = baseField.Get("m_Resource");
 
@@ -167,7 +208,7 @@ namespace eo_music
             using (var stream = new MemoryStream())
             using (var writer = new AssetsFileWriter(stream))
             {
-                
+
                 og_bun.file.Write(writer, bunReplacers);
                 //Console.WriteLine($"Scanning for {source_path}...");
                 //long FileDataOffset = og_bun.file.bundleHeader6.GetFileDataOffset();
@@ -212,7 +253,7 @@ namespace eo_music
                 Console.WriteLine("Unloading everything...");
                 am.UnloadAll();
 
-                
+
                 // Write the new bundle to the output
                 Console.WriteLine($"Saving to {output_bundle}...");
                 string output_path = Path.GetDirectoryName(output_bundle);
@@ -227,7 +268,7 @@ namespace eo_music
         {
             List<int> indexes = new List<int>();
             Console.WriteLine("Scanning for FSB5 Headers...");
-            for(int i = 0; i < data.Length; i++)
+            for (int i = 0; i < data.Length; i++)
             {
                 int ahead = data.Length - i;
                 if (ahead < 4)
@@ -237,7 +278,7 @@ namespace eo_music
                     indexes.Add(i);
             }
 
-            if(indexes.Count > 1)
+            if (indexes.Count > 1)
             {
                 Console.WriteLine("Found multiple FSB5 Headers! Cutting out everything between the first and the last...");
                 List<byte> res = data.ToList();
@@ -248,21 +289,18 @@ namespace eo_music
 
         static void Main(string[] args)
         {
-            //string input_bundle = @"D:\Games\Etrian Odyssey Origins Collection\Etrian Odyssey HD\backups\Etrian Odyssey_Data\StreamingAssets\aa\StandaloneWindows64\bgm_assets_town_day_1_18a4cd70dabe2b7c5fbb6b3db2568a4d.bundle";
-            //string output_bundle = @"D:\Games\Etrian Odyssey Origins Collection\Etrian Odyssey HD\mods\Test\Etrian Odyssey_Data\StreamingAssets\aa\StandaloneWindows64\bgm_assets_town_day_1_18a4cd70dabe2b7c5fbb6b3db2568a4d.bundle";
-            //string input_track = @"C:\Users\Random\Music\Kagerou (44100).mp3";
+            Console.Write("Do you want to output FSB headers info as a JSON file? (y/N) ");
+            bool outputJson = Console.ReadLine().ToLower() == "y";
 
-            ////AudioInfo inf = new AudioInfo
-            ////{
-            ////    sample_rate = 48000,
-            ////    samples = 11397888,
-            ////    channels = 2,
-            ////    bits_per_second = 16
-            ////};
-
-            ////PatchAudioBundle(input_a_bundle, input_fsb_path, inf, output_a_bundle);
-
-            ////return;
+            if (outputJson)
+            {
+                string jsonInputBundle = GetValidFilePath("Enter the music bundle file path (you can just drag n drop it in here): ").Replace("\"", "");
+                // Output FSB headers info as JSON
+                var fsbHeadersInfo = GetFSBHeadersInfo(File.ReadAllBytes(jsonInputBundle));
+                string json = JsonConvert.SerializeObject(fsbHeadersInfo, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText("fsb_headers_info.json", json);
+                Console.WriteLine("FSB headers info has been saved as fsb_headers_info.json.");
+            }
 
             string input_bundle = GetValidFilePath("Enter the original music bundle file path (you can just drag n drop it in here): ").Replace("\"", "");
             string input_track = GetValidFilePath("Enter your music file (drag n drop or type it in like a chad): ").Replace("\"", "");
@@ -275,58 +313,67 @@ namespace eo_music
 
             if (loop_setting)
             {
-                Console.Write("Enter loop start sample: ");
-                loop_start = int.Parse(Console.ReadLine());
-                Console.Write("Enter loop end sample (put in -1 if you want it to set the loop point to be the end of the song): ");
-                loop_end = int.Parse(Console.ReadLine());
-                
-                AudioInfo original_audio_info = GetAudioInfo(input_track);
-
-                if (loop_end == -1)
-                    loop_end = original_audio_info.samples - original_audio_info.sample_rate; // Set the end loop to be total time - 1 second
-
-                if(original_audio_info.sample_rate != TARGET_SAMPLE_RATE)
                 {
-                    Console.WriteLine($"Updating loop samples (from {original_audio_info.sample_rate} to {TARGET_SAMPLE_RATE})...");
-                    float diffHz = (float)TARGET_SAMPLE_RATE / (float)(original_audio_info.sample_rate);
-                    loop_start = (int)((float)loop_start * diffHz);
-                    loop_end = (int)((float)loop_end * diffHz);
+                    {
+                        Console.Write("Enter loop start sample: ");
+                        loop_start = int.Parse(Console.ReadLine());
+                        Console.Write("Enter loop end sample (put in -1 if you want it to set the loop point to be the end of the song): ");
+                        loop_end = int.Parse(Console.ReadLine());
+
+                        AudioInfo original_audio_info = GetAudioInfo(input_track);
+
+                        if (loop_end == -1)
+                            loop_end = original_audio_info.samples - original_audio_info.sample_rate; // Set the end loop to be total time - 1 second
+
+                        if (original_audio_info.sample_rate != TARGET_SAMPLE_RATE)
+                        {
+                            Console.WriteLine($"Updating loop samples (from {original_audio_info.sample_rate} to {TARGET_SAMPLE_RATE})...");
+                            float diffHz = (float)TARGET_SAMPLE_RATE / (float)(original_audio_info.sample_rate);
+                            loop_start = (int)((float)loop_start * diffHz);
+                            loop_end = (int)((float)loop_end * diffHz);
+                        }
+                    }
+
+                    Console.Write("Enter save path of new bundle (must include <filename>.bundle in the end): ");
+                    string output_bundle = Console.ReadLine().Replace("\"", "");
+
+                    // Output FSB headers info as JSON
+                    var fsbHeadersInfo = GetFSBHeadersInfo(File.ReadAllBytes(input_bundle));
+                    string json = JsonConvert.SerializeObject(fsbHeadersInfo, Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText("fsb_headers_info.json", json);
+
+                    if (File.Exists(OGG_TMP_PATH))
+                        File.Delete(OGG_TMP_PATH);
+                    if (File.Exists(FSB_TMP_PATH))
+                        File.Delete(FSB_TMP_PATH);
+
+                    Console.WriteLine();
+
+                    Console.WriteLine($"Converting {input_track} to ogg for remuxing...");
+                    ConvertFile(input_track, OGG_TMP_PATH);
+
+                    Console.WriteLine("Remuxing ogg into a fsb for injecting into bundle...");
+                    if (loop_setting)
+                        RemuxToFSB(OGG_TMP_PATH, FSB_TMP_PATH, loop_start, loop_end);
+                    else
+                        RemuxToFSB(OGG_TMP_PATH, FSB_TMP_PATH);
+
+                    Console.WriteLine("Getting ogg audio info for asset modification...");
+                    AudioInfo audio_info = GetAudioInfo(OGG_TMP_PATH);
+
+                    if (PatchAudioBundle(input_bundle, FSB_TMP_PATH, audio_info, output_bundle))
+                        Console.WriteLine("Success! Your new custom bundle with your own track has been made!");
+                    else
+                        Console.WriteLine("The function didn't return a true, so something probably went wrong!");
+
+                    if (File.Exists(OGG_TMP_PATH))
+                        File.Delete(OGG_TMP_PATH);
+                    if (File.Exists(FSB_TMP_PATH))
+                        File.Delete(FSB_TMP_PATH);
+
+                    Console.ReadLine();
                 }
             }
-
-            Console.Write("Enter save path of new bundle (must include <filename>.bundle in the end): ");
-            string output_bundle = Console.ReadLine().Replace("\"", "");
-
-            if (File.Exists(OGG_TMP_PATH))
-                File.Delete(OGG_TMP_PATH);
-            if (File.Exists(FSB_TMP_PATH))
-                File.Delete(FSB_TMP_PATH);
-            
-            Console.WriteLine();
-
-            Console.WriteLine($"Converting {input_track} to ogg for remuxing...");
-            ConvertFile(input_track, OGG_TMP_PATH);
-
-            Console.WriteLine("Remuxing ogg into a fsb for injecting into bundle...");
-            if (loop_setting)
-                RemuxToFSB(OGG_TMP_PATH, FSB_TMP_PATH, loop_start, loop_end);
-            else
-                RemuxToFSB(OGG_TMP_PATH, FSB_TMP_PATH);
-
-            Console.WriteLine("Getting ogg audio info for asset modification...");
-            AudioInfo audio_info = GetAudioInfo(OGG_TMP_PATH);
-
-            if (PatchAudioBundle(input_bundle, FSB_TMP_PATH, audio_info, output_bundle))
-                Console.WriteLine("Success! Your new custom bundle with your own track has been made!");
-            else
-                Console.WriteLine("The function didn't return a true, so something probably went wrong!");
-
-            if (File.Exists(OGG_TMP_PATH))
-                File.Delete(OGG_TMP_PATH);
-            if (File.Exists(FSB_TMP_PATH))
-                File.Delete(FSB_TMP_PATH);
-
-            Console.ReadLine();
         }
     }
 }
